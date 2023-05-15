3154026238f5260d3f905214da7facb3b3e669d5
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2023-05-15 09:20:33 -0400

    Update Greeter.sol

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,7 +5,7 @@ contract Greeter {
     
     string public greeting;
 
-    function setGreeting(string memory _greeting) public { //calldata is immutable cheaper than memory. 
+    function setGreeting(string calldata _greeting) public { //calldata is immutable cheaper than memory. 
         greeting = _greeting;
     }
     
